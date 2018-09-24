7da100a9f9944fa701d800ba6e96bf0be6b6ec6d
Rahul <Rahul@HP>
2018-09-24 16:15:10 +0530

    Information to setup Database schema for CRUD

---
+++ web/database-info/users.sql
@@ -0,0 +1,26 @@
+SQL> select * from users;
+
+        ID NAME                           EMAIL                                 
+---------- ------------------------------ ------------------------------        
+PASSWORD                       GENDER     ADDRESS                               
+------------------------------ ---------- ------------------------------        
+       100 Rahul Pathak                   rp@gmail.com                          
+qwerty                         Male       Greater Noida                         
+                                                                                
+       102 Sheela Dixit                   sd@gmail.com                          
+plmokn                         Female     Lok Sabha, Delhi                      
+                                                                                
+       103 Kuldeep Singh                  ks@gmail.com                          
+big data                       Male       Ghaziabad                             
+                                                                                
+
+        ID NAME                           EMAIL                                 
+---------- ------------------------------ ------------------------------        
+PASSWORD                       GENDER     ADDRESS                               
+------------------------------ ---------- ------------------------------        
+       101 Virat Kohli                    vk@gmail.com                          
+anushka                        Male       Lajpat Nagar, Delhi                   
+                                                                                
+       104 Narendra Modi                  modi@gmail.com                        
+acchedin                       Male       Ahmedabad, Gujrat                     
+               
